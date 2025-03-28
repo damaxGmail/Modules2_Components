99bb8d649b51ecf2e7e303f272c80b274c65aec2
Damiriko <damiriko80@gmail.com>
2025-03-28 14:49:37 +0300

    add buttons

--- Zadanie3/src/App.js
+++ Zadanie3/src/App.js
@@ -1,15 +1,29 @@
 import styles from './app.module.css';
-import data from './data.json';
 import { useState } from 'react';
 
 export const App = () => {
+	const ourDigitals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 	return (
 		<div className={styles.container}>
-			<div className={styles.card}>
-				<h1>Калькулятор</h1>
+			<div className={styles.screen}>
 
 			</div>
-		</div >
+			<div className={styles.buttons}>
+				<div className={`${styles.button} ${styles.button__digital}`}>
+					<div className={styles.button__digital}>
+						CE/C
+					</div>
+					{ourDigitals.map(num => (
+						<div key={num} className={styles.button__digital}>
+							{num}
+						</div>
+					))}
+				</div>
+				<div className={`${styles.button} ${styles.button__operand}`}>
+
+				</div>
+			</div>
+		</div>
 	);
 };
